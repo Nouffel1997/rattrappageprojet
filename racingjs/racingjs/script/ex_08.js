e1e94f5fbd54c1e3bdc65e0822f1504ae9003fd3
Merhdaoui Nouffel <nouffel1.merhdaoui@epitech.eu>
2020-03-22 02:12:13 +0100

    derniere modif des projets rattrapages racingjs maquette-introduction et css_generator

--- racingjs/racingjs/script/ex_08.js
+++ racingjs/racingjs/script/ex_08.js
@@ -1,63 +1,49 @@
-window.onload = function()
-{
-	var y = document.all;
-	console.log(y);
+window.onload = function () {
+    var x = document.all[14].children;
 
-	var x = document.all[14].children;
-
-    var orange = 0; 
-    var purple = 0; 
-    var noir = 0; 
+    var orange = 0;
+    var purple = 0;
+    var noir = 0;
     var olive = 0;
 
-    for(i = 0 ; i < x.length ; i++)
-    {
-        
-        var test = x[i];
-        var test = window.getComputedStyle(test, null); // donne la valeur calculee finale de toutes les proprietes css 
+    for (i = 0; i < x.length; i++) {
+
+        var canvas = x[i];
+        var test = window.getComputedStyle(canvas, null); // donne la valeur calculee finale de toutes les proprietes css 
         var property = test.getPropertyValue('background-color');
 
-        if(property == 'rgb(255, 165, 0)')
-        {
-			orange++;
-		}
-        if(property == 'rgb(128, 0, 128)')
-        {
+        if (property == 'rgb(255, 165, 0)') {
+            orange++;
+        }
+        if (property == 'rgb(128, 0, 128)') {
             purple++;
-		}
-		if(property == 'rgb(128, 128, 0)')
-        {
+        }
+        if (property == 'rgb(128, 128, 0)') {
             olive++;
         }
-        if(property == 'rgb(0, 0, 0)')
-        {
-            noir++;   
+        if (property == 'rgb(0, 0, 0)') {
+            noir++;
         }
 
     }
 
-    for(i = 0 ; i < x.length; i++)
-    {
+    for (i = 0; i < x.length; i++) {
 
-		for( let i = orange + purple + noir ; i < orange + purple + noir + olive ; i++)
-        {
-            test = x[i]
-            test.setAttribute('style', 'background-color : olive !important');
-		}
-		for( let i = orange ; i < orange + purple ; i++)
-        {
-            test = x[i]
-            test.setAttribute('style', 'background-color : purple !important');
+        for (let i = orange + purple + noir; i < orange + purple + noir + olive; i++) {
+            canvas = x[i]
+            canvas.setAttribute('style', 'background-color : olive !important');
         }
-        for (let i = 0; i < orange; i++) 
-        {
-            test = x[i];
-            test.setAttribute('style', 'background-color : orange !important');
+        for (let i = orange; i < orange + purple; i++) {
+            canvas = x[i]
+            canvas.setAttribute('style', 'background-color : purple !important');
         }
-        for( let i = orange + purple ; i < orange + purple + noir ; i++)
-        {
-            test = x[i]
-            test.setAttribute('style', 'background-color : black !important');
+        for (let i = 0; i < orange; i++) {
+            canvas = x[i];
+            canvas.setAttribute('style', 'background-color : orange !important');
+        }
+        for (let i = orange + purple; i < orange + purple + noir; i++) {
+            canvas = x[i]
+            canvas.setAttribute('style', 'background-color : black !important');
         }
 
     }
--- racingjs/racingjs/script/ex_09.js
+++ racingjs/racingjs/script/ex_09.js
@@ -1,61 +1,56 @@
 window.onload = function () {
-    var x = document.all;
-    console.log(x);
+
 
     canvas = document.all[17];
     carre = document.all[16];
 
     canvas.setAttribute("draggable", true);
-    canvas.setAttribute("ondragstart", "testou(event)");
-    carre.setAttribute("ondragover", "test(event)");
-    carre.setAttribute("ondrop", "testi(event)");
-   
+    canvas.setAttribute("ondragstart", "start(event)");
+    carre.setAttribute("ondragover", "move(event)");
+    carre.setAttribute("ondrop", "drop(event)");
+
     canvas.style.position = "relative";
 
-    no = canvas.getBoundingClientRect(); // renvoie la taille d'un element et sa position relative 
-    
+    heightposition = canvas.getBoundingClientRect(); // renvoie la taille d'un element et sa position relative 
+
 };
 
-function test(test){
+function move(test) {
     test.preventDefault();
 }
 
-function testi(test) {
-
-    test.preventDefault(); //si l'événement n'est pas traité sont ation de base ne sera pas prise en compte
-
-    var e = test.dataTransfer.getData("text");
+function drop(test) {
 
     var x = test.clientX;
-    var y = test.clientY; 
+    var y = test.clientY;
 
-    resX = x - no.left - dx;
-    resY = y - no.top - dy;
+    resX = x - heightposition.left - dx;
+    resY = y - heightposition.top - dy;
 
     canvas.style.top = resY + "px";
     canvas.style.left = resX + "px";
 
-    aziz(resX, resY);
+    coordonne(resX, resY);
 }
 
-function testou(test) {
+function start(test) {
 
-    test.dataTransfer.setData("text", test.target.soucis); //defini les actions de dragdata 
 
-    var no1 = canvas.getBoundingClientRect();// renvoie la taille d'un element et sa position relative 
-    
+
+    var heightposition1 = canvas.getBoundingClientRect();
+
     var x = test.clientX; //action x de la souris    
     var y = test.clientY; //action y de la souris 
 
-    dx = x - no1.left; // action de la souris - la taille de gauche 
-    dy = y - no1.top; //action de la souris - la taille d'en haut
+    dx = x - heightposition1.left; // action de la souris - la taille de gauche 
+    dy = y - heightposition1.top; //action de la souris - la taille d'en haut
 }
 
 
 
-function aziz(x, y){
-    document.all[18].innerHTML = 
-    "Nouvelles coordonnées => {x:" + x + "px" + ", y:" + y + "px" +"}";
+function coordonne(x, y) {
+    document.all[18].innerHTML =
+        "Nouvelles coordonnées => {x:" + x + "px" + ", y:" + y + "px" + "}";
 }
 
 
